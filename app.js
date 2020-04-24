a746d1950657ed377ac2d37b8779c0b14fe04e90
Tiffani Hess <hesstjune@gmail.com>
2020-04-24 09:40:06 -0500

    changed functions to async so first API loads before second

--- app.js
+++ app.js
@@ -20,43 +20,51 @@ const day = String(date.getDate());
 const year = String(date.getFullYear()).substr(-2);
 const today = `${month}/${day}/${year}`;
 
-corona.getData().then((data) => {
-  data.forEach((result) => {
-    if (result.county === "black hawk") {
-      const cases = result.timeline.cases;
-      console.log(cases);
-      Object.entries(cases).forEach(([key, value]) => {
-        //Don't get today's key from this API, mathdroid is updated sooner
-        if (key !== today) {
-          output += `<tr>
+async function firstAPI() {
+  corona.getData().then((data) => {
+    data.forEach((result) => {
+      if (result.county === "black hawk") {
+        const cases = result.timeline.cases;
+        console.log(cases);
+        Object.entries(cases).forEach(([key, value]) => {
+          //Don't get today's key from this API, mathdroid is updated sooner
+          if (key !== today) {
+            output += `<tr>
       <td class="date">${key}</td>
       <td class="cases">${value}</td>
       <td class="count">${value - totalCount}</td>
     </tr>`;
-          totalCount = value;
-        }
-      });
-    }
+            totalCount = value;
+          }
+        });
+      }
+    });
   });
-});
+  return;
+}
 
-corona.getCounties().then((data) => {
-  data.forEach((result) => {
-    if (result.provinceState === "Iowa") {
-      if (result.admin2 === "Black Hawk") {
-        // console.log(result);
-        output += `
+async function secondAPI() {
+  await firstAPI();
+  corona.getCounties().then((data) => {
+    data.forEach((result) => {
+      if (result.provinceState === "Iowa") {
+        if (result.admin2 === "Black Hawk") {
+          // console.log(result);
+          output += `
         <td class="date">${today}</td>
         <td class="cases">${result.confirmed}</td>
        <td class="count">${result.confirmed - totalCount}</td>`;
-        totalCount = result.confirmed;
+          totalCount = result.confirmed;
+        }
       }
-    }
-  });
-  output += `</tbody>
+    });
+    output += `</tbody>
 </table>`;
-  results.innerHTML = output;
-});
+    results.innerHTML = output;
+  });
+}
+
+secondAPI();
 
 // Calling to separate API to get today's cases because it's updated sooner
 // corona.getCounties().then((data) => {
