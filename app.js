d2f918f126cad1b22a8a4a4487ad7d1fb7ba7c30
Tiffani Hess <hesstjune@gmail.com>
2020-04-26 11:31:58 -0500

    changed api calls to not be async

--- app.js
+++ app.js
@@ -20,51 +20,45 @@ const day = String(date.getDate());
 const year = String(date.getFullYear()).substr(-2);
 const today = `${month}/${day}/${year}`;
 
-async function firstAPI() {
-  corona.getData().then((data) => {
-    data.forEach((result) => {
-      if (result.county === "black hawk") {
-        const cases = result.timeline.cases;
-        console.log(cases);
-        Object.entries(cases).forEach(([key, value]) => {
-          //Don't get today's key from this API, mathdroid is updated sooner
-          if (key !== today) {
-            output += `<tr>
+// Get today's cases from mathdroid
+corona.getCounties().then((data) => {
+  data.forEach((result) => {
+    if (result.provinceState === "Iowa") {
+      if (result.admin2 === "Black Hawk") {
+        // console.log(result);
+        output += `
+        <td class="date">${today}</td>
+        <td class="cases">${result.confirmed}</td>
+       <td class="count">${result.confirmed - totalCount}</td>`;
+        totalCount = result.confirmed;
+      }
+    }
+  });
+  output += `</tbody>
+</table>`;
+  results.innerHTML = output;
+});
+
+//Get historical cases from corona api
+corona.getData().then((data) => {
+  data.forEach((result) => {
+    if (result.county === "black hawk") {
+      const cases = result.timeline.cases;
+      console.log(cases);
+      Object.entries(cases).forEach(([key, value]) => {
+        //Don't get today's key from this API, mathdroid is updated sooner
+        if (key !== today) {
+          output += `<tr>
       <td class="date">${key}</td>
       <td class="cases">${value}</td>
       <td class="count">${value - totalCount}</td>
     </tr>`;
-            totalCount = value;
-          }
-        });
-      }
-    });
-  });
-  return;
-}
-
-async function secondAPI() {
-  await firstAPI();
-  corona.getCounties().then((data) => {
-    data.forEach((result) => {
-      if (result.provinceState === "Iowa") {
-        if (result.admin2 === "Black Hawk") {
-          // console.log(result);
-          output += `
-        <td class="date">${today}</td>
-        <td class="cases">${result.confirmed}</td>
-       <td class="count">${result.confirmed - totalCount}</td>`;
-          totalCount = result.confirmed;
+          totalCount = value;
         }
-      }
-    });
-    output += `</tbody>
-</table>`;
-    results.innerHTML = output;
+      });
+    }
   });
-}
-
-secondAPI();
+});
 
 // Calling to separate API to get today's cases because it's updated sooner
 // corona.getCounties().then((data) => {
